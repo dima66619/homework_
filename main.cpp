ee3cd756c2e11d8f9f9549c74629144248008478
tdreg <tdreg@DESKTOP-E6T222J>
2024-09-15 04:14:21 +0300

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	int a, b, * ptr_a, * ptr_b;
+
+	cout << "Enter two numbers: ";
+
+	cin >> a >> b;
+
+
+	ptr_a = &a;
+
+	ptr_b = &b;
+
+
+	if (*ptr_a > *ptr_b) {
+
+		cout << " first number whats equal:" << *ptr_a << ", greater than second" << endl;
+
+	}
+	else {
+		cout << " second number whats equal:" << *ptr_b << ", greater than first" << endl;
+
+	}
+
+	return 0;
+
+}
